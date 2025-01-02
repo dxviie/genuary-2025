f96189e24b80faae20451d109f86b28ed124126a
david <david@d17e.dev>
2025-01-02 17:25:38 +0100

    Prompt 01. Calling it done. For now.

--- src/lib/Prompt01.svelte
+++ src/lib/Prompt01.svelte
@@ -14,36 +14,40 @@
 		lines: Line[];
 		fx: TransformFunction;
 		fy: TransformFunction;
-		fr: TransformFunction;
 	};
 
 	let styles = $state(['']);
 	const R = 250;
 	let animationFrame = -1;
-	const LINE_GROUPS = generateLineGroups();
+	const LINE_GROUPS = $state(generateLineGroups());
 
 	function generateLineGroups() {
 		const minWidth = 0.5;
-		const maxWidth = 5;
-		const minSpacing = 5;
+		const maxWidth = 10;
+		const minSpacing = 5 + (Math.random() * 15);
 		const maxSpacing = 50;
-		const horizontal = false;
+		const allowCrossing = Math.random() > 0.8;
+		let horizontal = Math.random() > 0.5;
 		let currentWidth = Math.random() * (maxWidth - minWidth) + minWidth;
 		let currentSpacing = Math.random() * (maxSpacing - minSpacing) + minSpacing;
 		let availableSpace = currentSpacing;
 		let start = 0;
 		const groups: LineGroup[] = [];
 		while (availableSpace > 0) {
+			const maskCx = WIDTH / 2 + (Math.random() * 2 - 1) * (R / 2);
+			const maskCy = HEIGHT / 2 + (Math.random() * 2 - 1) * (R / 2);
 			groups.push({
 				index: groups.length,
-				mask: createMask(`mask-${groups.length}`, WIDTH / 2, HEIGHT / 2, R),
+				mask: createMask(`mask-${groups.length}`, maskCx, maskCy, R),
 				lines: generateLines(horizontal, currentSpacing, currentWidth, start),
 				fx: createAnimationFunction(Math.sin, Math.random() * 2500 + 1000, 250),
-				fy: createAnimationFunction(Math.cos, Math.random() * 2500 + 1000, 50),
-				fr: createAnimationFunction(() => 1, 1)
+				fy: createAnimationFunction(Math.cos, Math.random() * 2500 + 1000, 50)
 			});
 			availableSpace -= currentWidth;
 			start += currentWidth;
+			if (allowCrossing) {
+				horizontal = Math.random() > 0.5;
+			}
 
 			currentWidth = Math.random() * (maxWidth - minWidth) + minWidth;
 			currentSpacing = Math.random() * (availableSpace - minSpacing) + minSpacing;
@@ -75,10 +79,9 @@
 		return (t: DOMHighResTimeStamp) => f(t / timeRatio) * amplitude;
 	}
 
-	function createTransform(time: DOMHighResTimeStamp, fx: TransformFunction, fy: TransformFunction, fr: TransformFunction) {
+	function createTransform(time: DOMHighResTimeStamp, fx: TransformFunction, fy: TransformFunction) {
 		const cx = fx(time);
 		const cy = fy(time);
-		const r = fr(time);
 		return `transform: translate(${cx}px, ${cy}px)`;
 	}
 
@@ -86,7 +89,7 @@
 		const now = performance.now();
 		const elapsed = now - ZERO;
 		for (let i = 0; i < LINE_GROUPS.length; i++) {
-			styles[i] = createTransform(elapsed, LINE_GROUPS[i].fx, LINE_GROUPS[i].fy, LINE_GROUPS[i].fr);
+			styles[i] = createTransform(elapsed, LINE_GROUPS[i].fx, LINE_GROUPS[i].fy);
 		}
 		animationFrame = requestAnimationFrame(animate);
 	}
@@ -115,8 +118,8 @@
 	<defs>
 		{#each LINE_GROUPS as group}
 			<mask id={group.mask.id}>
-				<circle r={group.mask.r} cx={group.mask.cx} cy={group.mask.cy} stroke="black" stroke-width="0" fill="#FFF"
-								style={styles[group.index]} />
+				<rect height={group.mask.r * 2} width={group.mask.r * 2} x={group.mask.cx - group.mask.r} y={group.mask.cy - group.mask.r}
+							fill="#FFF" style={styles[group.index]} />
 			</mask>
 		{/each}
 	</defs>
